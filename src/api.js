674942d4dcc489fad486f0ecb36a04ff00081961
Agnieszka Majmurek <majmurek.agnieszka@gmail.com>
2024-10-25 22:59:12 +0200

    .

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
+const cors = require("cors");
 const router = express.Router();
 
 let records = [];
-
+app.use(cors());
 
 router.get('/', (req, res) => {
   res.send('App is running..');
